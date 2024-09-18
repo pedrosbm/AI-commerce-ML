5adb39338c89fe2eb9c538112e37c0cc3b96237b
pedro <pedrosbmartinazzo@hotmail.com>
2024-09-17 23:33:39 -0300

    configuração de cors pra apresentação da banda

--- api/app.py
+++ api/app.py
@@ -1,15 +1,17 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
 import pandas as pd
 
 app = Flask(__name__)
+CORS(app)  # Configura o CORS para aceitar requisições de qualquer origem
 
 with open('api\knn_model1.pkl', 'rb') as model_file:
     knn_model = pickle.load(model_file)
 
 products_pivot = pd.read_csv('api\products_data.csv', index_col=0)
 
-@app.route('/recommend', methods=['POST'])
+@app.route('/recommend', methods=['POST'])  
 def recommend():
     data = request.json
     product_name = data['product_name']
